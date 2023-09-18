eccd16bbfa2f6badcc12e92b9347e7f0bbb01f92
Mayas Fares <cph-mf337@cphbusiness.dk>
2023-09-18 20:15:49 +0200

    first commit

---
+++ src/main/java/org/example/BookScrapper.java
@@ -0,0 +1,42 @@
+package org.example;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class BookScrapper {
+    public static void main(String[] args) throws IOException {
+
+        String url = "https://books.toscrape.com/";
+
+        try{
+            Document document = Jsoup.connect(url).get();
+            Elements books = document.select(".product_pod");
+
+            System.out.println("==========================");
+            System.out.println("Books - Web Scrapper");
+            System.out.println("==========================");
+            for(Element bk:books) {
+                String title = bk.select("h3> a").text();
+                String price = bk.select(".price_color").text();
+                String actual_price = price.substring(1);
+
+                // Get books less than £20
+
+                if(Double.parseDouble(actual_price) < 20.0)
+                System.out.println(title + "-" + price);
+
+            }
+
+            System.out.println("=============================");
+
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
